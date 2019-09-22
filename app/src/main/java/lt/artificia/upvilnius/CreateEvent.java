140590ab0e1edc4cebff2ed54512fc0983a4bf38
Vykintas Valuzis <vykintazo@gmail.com>
2019-09-22 15:09:10 +0300

    result

--- app/src/main/java/lt/artificia/upvilnius/CreateEvent.java
+++ app/src/main/java/lt/artificia/upvilnius/CreateEvent.java
@@ -2,6 +2,7 @@ package lt.artificia.upvilnius;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 
@@ -12,6 +13,8 @@ import static java.security.AccessController.getContext;
 public class CreateEvent extends Activity {
 
     String[] List1 = new String[] {"2", "3", "4", "5", "6", "7", "8", "9", "10"};
+    String[] List2 = new String[] {"Basketball", "Football", "Baseball", "Tennis", "Other"};
+    String[] List3 = new String[] {"2", "3", "4", "5", "6", "7", "8", "9", "10"};
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,5 +26,16 @@ public class CreateEvent extends Activity {
 
         AutoCompleteTextView editTextFilledExposedDropdown = findViewById(R.id.filled_exposed_dropdown1);
         editTextFilledExposedDropdown.setAdapter(adapter);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this,
+                R.layout.dropdown_menu_popup_item,
+                List2);
+
+        AutoCompleteTextView editTextFilledExposedDropdown2 = findViewById(R.id.filled_exposed_dropdown2);
+        editTextFilledExposedDropdown2.setAdapter(adapter2);
+
+    }
+
+    public void close(View v){
+        finish();
     }
 }
